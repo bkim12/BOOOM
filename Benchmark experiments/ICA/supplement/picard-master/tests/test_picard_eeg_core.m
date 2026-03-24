30f78db732490f2f88b042608f317bae57c08c6e
bkim12 <beomchang.kim13@gmail.com>
2026-03-24 18:49:12 -0400

    Delete Benchmark experiments/ICA/supplement/picard-master directory

--- Benchmark experiments/ICA/supplement/picard-master/tests/test_picard_eeg_core.m
+++
@@ -1,3 +0,0 @@
-EEG = pop_loadset('eeglab_data_with_ica_tmp.set');
-
-EEG = eeg_picard(EEG, 'w_init', eye(32));
--- Benchmark experiments/ICA/supplement/picard-master/tests/test_whitening.m
+++
@@ -1,5 +0,0 @@
-EEG = pop_loadset('eeglab_data_with_ica_tmp.set');
-
-[~,whitening_matrix] = whitening(double(EEG.data), 'pca', EEG.nbchan);
-
-whitening_matrix(1:6,1:6)
